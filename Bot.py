c59d499ebc84bd2fdb2474f1e84022cc07ee534a
Younes RM <rastgary@coventry.ac.uk>
2018-10-31 13:24:26 +0000

    Initial commit

---
+++ Bot.py
@@ -0,0 +1,13 @@
+import discord
+import asyncio
+import requests
+import http.client
+import json
+import urllib.request
+from discord.ext import commands
+import sqlite3
+from Includes import test
+
+test.say("Hello World")
+
+
