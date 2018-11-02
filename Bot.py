d57974a8c4b29d8f796f621edfbb970d5042880e
Younes RM <rastgary@coventry.ac.uk>
2018-11-02 15:19:19 +0000

    using register command now your able to register and save your information in the database

--- Bot.py
+++ Bot.py
@@ -2,10 +2,10 @@ import discord
 import asyncio
 from discord.ext import commands
 import sqlite3
+import db as database
 
 
 #Younes RM Code Section
-
 with sqlite3.connect("BotDB.db") as db: 
     c = db.cursor()
 client = discord.Client()
@@ -14,14 +14,22 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    ma = message.author
   
     if message.author == client.user:
         return
       
 
     if message.content.startswith('!register'):
-        ma = message.author
+        
         await rf(ma)
+        
+    if message.content.startswith('!all'):
+        await sendmsg(ma, ma)
+        await allusers(ma) 
+        
+    if message.content.startswith('!delete'):
+        await client.delete_message(message)
      
 
 
@@ -29,32 +37,50 @@ async def on_message(message):
     
 
 
+
     
-@client.event
+@client.event   
+async def allusers(ma):
+    
+    data = ("SELECT * from users")
+    c.execute(data)
+    results = c.fetchall()
+    for row in results:
+        msg = ("name: " + row[1] + " / Discord ID: " + row[3])
+        await sendmsg(ma, msg)
+    
 
 async def rf(ma):
     msg = 'Hello, to register please Chose a Username.'
-    username = await getmsg(ma, msg)
+    username = await getmsg(ma, msg, False)
     if username is None:
         await timesup(ma)
     else:
         msg = 'Thank you. Now please enter a memorable word'
-        pw = await getmsg(ma, msg)
+        pw = await getmsg(ma, msg, True)
         if pw is None:
             await timesup(ma)
         else:   
             msg= 'Thank you, just to make sure please enter your memorable word once again'
-            cpw = await getmsg(ma, msg)
+            cpw = await getmsg(ma, msg, True)
             if cpw is None:
                 await timesup(ma)
             else:   
                 if pw == cpw:
+                    await register(username,pw,ma)
                     await sendmsg(ma, 'All Good')
-                    print(username +"/"+ pw + "/"+ cpw)
                 else:
                     await sendmsg(ma, 'Password did not match')
-                    print(pw)
-                    print(cpw)
+                    
+
+                
+
+                
+async def register(username,pw,ma):
+    ma = str(ma)
+    data = ("INSERT INTO users(username, passwrod, discord_id, login) VALUES(?, ?, ?, 0)")
+    c.execute(data, [(username),(pw),(ma)])  
+    db.commit()
 
                 
                 
@@ -62,10 +88,12 @@ async def sendmsg(ma, msg):
     await client.send_message(ma, msg) 
     
 
-async def getmsg(ma,msg):
+async def getmsg(ma,msg,d):
     await client.send_message(ma, msg)
     answer = await client.wait_for_message(timeout=10.0, author=ma)
-    return answer.content
+    a = answer.content
+    
+    return a
     
 async def timesup(ma):
     msg = 'You have took to long to responde. use !command to start again.'
