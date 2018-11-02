82b9289927feaedc3f2bacc96a60b971fc85438d
Younes RM <rastgary@coventry.ac.uk>
2018-11-02 10:37:34 +0000

    Please Pull from master to start

--- Bot.py
+++ Bot.py
@@ -1,6 +1,41 @@
-from Includes.include import *
+import discord
+import asyncio
+from discord.ext import commands
+import sqlite3
 
-usm.login("younesrm#638")
+
+with sqlite3.connect("BotDB.db") as db:  
+    c = db.cursor()
+client = discord.Client()
 
 
 
+@client.event
+async def on_message(message):
+  
+    if message.author == client.user:
+      return
+      
+
+    if message.content.startswith('!register'):
+      ma = message.author
+      await usm.kk(ma)
+      await sendmsg(ma ,dm)
+     
+
+
+
+    
+    
+@client.event  
+async def sendmsg(ma, msg):
+  await client.send_message(ma, msg) 
+  
+    
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')  
+client.run('NTA1MTYyODI0MjEzODU2MjU2.DrRxzA.AejgQIuwCIx6WGIZBFv0H-fGHrA')
--- Includes/include.py
+++
@@ -1,5 +0,0 @@
-import discord
-import asyncio
-from discord.ext import commands
-import sqlite3
-import Includes.login as usm
