a7f7a00d23c459b84d469066988dc31467ea37bb
Younes RM <rastgary@coventry.ac.uk>
2018-11-02 14:13:51 +0000

    register form has been added

--- Bot.py
+++ Bot.py
@@ -21,18 +21,55 @@ async def on_message(message):
 
     if message.content.startswith('!register'):
         ma = message.author
-        await usm.kk(ma)
-        await sendmsg(ma ,dm)
+        await rf(ma)
      
 
 
 
     
+
+
     
-@client.event  
+@client.event
+
+async def rf(ma):
+    msg = 'Hello, to register please Chose a Username.'
+    username = await getmsg(ma, msg)
+    if username is None:
+        await timesup(ma)
+    else:
+        msg = 'Thank you. Now please enter a memorable word'
+        pw = await getmsg(ma, msg)
+        if pw is None:
+            await timesup(ma)
+        else:   
+            msg= 'Thank you, just to make sure please enter your memorable word once again'
+            cpw = await getmsg(ma, msg)
+            if cpw is None:
+                await timesup(ma)
+            else:   
+                if pw == cpw:
+                    await sendmsg(ma, 'All Good')
+                    print(username +"/"+ pw + "/"+ cpw)
+                else:
+                    await sendmsg(ma, 'Password did not match')
+                    print(pw)
+                    print(cpw)
+
+                
+                
 async def sendmsg(ma, msg):
     await client.send_message(ma, msg) 
     
+
+async def getmsg(ma,msg):
+    await client.send_message(ma, msg)
+    answer = await client.wait_for_message(timeout=10.0, author=ma)
+    return answer.content
+    
+async def timesup(ma):
+    msg = 'You have took to long to responde. use !command to start again.'
+    await sendmsg(ma, msg) 
     
 #Younes RM Code Section End
 
