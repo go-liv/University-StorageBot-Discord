1f7226a95c06a942f88e8459402283ae77682d36
Guilherme Miguel <deoliveg@coventry.ac.uk>
2018-11-02 10:20:46 +0000

    Test change

--- Includes/download.py
+++ Includes/download.py
@@ -5,7 +5,7 @@ import urllib.request
     define download(username, file path, file):
         knowing the username and the file name go to table and get file path
         with file path send message to the user with the file
-        let user know that discord has a function that allows the user to download the file to the pc
+        let user know that discord has a function that allows the user to download the file to the pc.
 
 """
 
