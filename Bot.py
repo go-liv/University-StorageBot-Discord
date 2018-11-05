59ca35a3f1513732bb68218f800acd04baf2e709
Younes RM <rastgary@coventry.ac.uk>
2018-11-05 15:24:53 +0000

    register, login, logout has been added including error handler and some security, however further update is needed on this class. but for now it all can be used to provide the needed information and functions to work and create your parts of codes such as download, upload, user interface and more

--- Bot.py
+++ Bot.py
@@ -22,8 +22,13 @@ async def on_message(message):
       
 
     if message.content.startswith('!register'):
-        
         await rf(ma)
+    
+    if message.content.startswith('!login'):
+        await login(ma)
+        
+    if message.content.startswith('!logout'):
+        await logout(ma)
         
     if message.content.startswith('!all'):
         await sendmsg(ma, ma)
@@ -31,7 +36,9 @@ async def on_message(message):
         
     if message.content.startswith('!delete'):
         await client.delete_message(message)
-     
+        
+    if message.content.startswith('!dm'):
+        await delete(ma)
 
 
 
@@ -51,20 +58,64 @@ async def allusers(ma):
         await sendmsg(ma, msg)
     
 
+async def login(ma):
+    if await checkdb('discord_id', ma) == False:
+        msg = 'You do not have an account with us yet'
+        await sendmsg(ma, msg)
+        return
+    elif await logged(ma) == True:
+        msg = 'You are Already logged in. use !logout to Logout from your account'
+        await sendmsg(ma, msg)
+    else:
+        data = ("UPDATE users SET login = 1 WHERE discord_id = ?")
+        c.execute(data, [(str(ma))])
+        db.commit()
+        await sendmsg(ma, 'You have been login')
+        
+async def logout(ma):
+    if await checkdb('discord_id', ma) == False:
+        msg = 'You do not have an account with us yet'
+        await sendmsg(ma, msg)
+        return
+    elif await logged(ma) == False:
+        msg = 'You are not logged in'
+        await sendmsg(ma, msg)
+        return
+    else:
+        data = ("UPDATE users SET login = 0 WHERE discord_id = ?")
+        c.execute(data, [(str(ma))])
+        db.commit()
+        await sendmsg(ma, 'You have been Logout')
+        
+async def logged(ma):
+    ma = str(ma)
+    islogged = ("SELECT * from users WHERE discord_id = ? AND login = 1")
+    c.execute(islogged, [(ma)])
+    results = c.fetchall()
+    if results:
+        return True
+    else:
+        return False
+
+
 async def rf(ma):
     msg = 'Hello, to register please Chose a Username.'
     username = await getmsg(ma, msg, False)
-    if username is None:
+    if username is False:
         await timesup(ma)
     else:
+        if await checkdb('username', username) == True:
+            msg = 'This username already exists! call the bot again'
+            await sendmsg(ma, msg)
+            return
         msg = 'Thank you. Now please enter a memorable word'
         pw = await getmsg(ma, msg, True)
-        if pw is None:
+        if pw is False:
             await timesup(ma)
         else:   
             msg= 'Thank you, just to make sure please enter your memorable word once again'
             cpw = await getmsg(ma, msg, True)
-            if cpw is None:
+            if cpw is False:
                 await timesup(ma)
             else:   
                 if pw == cpw:
@@ -76,13 +127,29 @@ async def rf(ma):
 
                 
 
-                
+async def checkdb(rn, rv):
+    rn = str(rn)
+    rv = str(rv)
+    data = ("SELECT * from users WHERE " + rn + " = ?")
+    c.execute(data, [(rv)])
+    results = c.fetchall()
+    if results:
+        return True
+    else:
+        return False
+
+
 async def register(username,pw,ma):
     ma = str(ma)
     data = ("INSERT INTO users(username, passwrod, discord_id, login) VALUES(?, ?, ?, 0)")
     c.execute(data, [(username),(pw),(ma)])  
     db.commit()
 
+async def delete(ma):
+    ma = str(ma)
+    data = ("DELETE FROM users WHERE discord_id = ?")
+    c.execute(data, [(ma)])  
+    db.commit()
                 
                 
 async def sendmsg(ma, msg):
@@ -92,9 +159,11 @@ async def sendmsg(ma, msg):
 async def getmsg(ma,msg,d):
     await client.send_message(ma, msg)
     answer = await client.wait_for_message(timeout=10.0, author=ma)
-    a = answer.content
-    
-    return a
+    if answer is None:
+        return False
+    else:
+        a = answer.content
+        return a
     
 async def timesup(ma):
     msg = 'You have took to long to responde. use !command to start again.'
