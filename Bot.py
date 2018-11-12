d32d9cb9a263e19269bdc9ab33464e4cf4fd25eb
Guilherme Miguel <deoliveg@coventry.ac.uk>
2018-11-12 16:54:08 +0000

    Added download code to Bot.py with template with each parts of the code belong to #Guilherme

--- Bot.py
+++ Bot.py
@@ -45,12 +45,18 @@ async def on_message(message):
     if message.content.startswith('!dm'):
         await delete(ma)
 
-    #Guilherme's Code
-    if message.content.startswith('!download'):
-        await download(ma)
-    
-    
-    #End of Guilherme's code
+    #####################-------Guilherme's Code-------###########################
+    #                                                                            #
+    #                                                                            #    
+    if message.content.startswith('!download'):                                  #
+        await download(ma)                                                       #
+    #                                                                            #        
+    #                                                                            #    
+    if message.content.startswith('!myfiles'):                                   #
+        await myfiles(ma)                                                        #
+    #                                                                            #
+    #                                                                            #
+    #####################----End of Guilherme's code----##########################
 
     
 
@@ -181,6 +187,68 @@ async def timesup(ma):
     await sendmsg(ma, msg) 
     
 #Younes RM Code Section End
+
+
+##################################################--------Guilherme's Code--------###################################################################
+#This function allows the user to download a file (it is the last command getting used, before this we have the upload function and a read function)#
+#                                                                                                                                                   #
+#                                                                                                                                                   #
+#                                                                                                                                                   #
+async def download(ma):                                                                                                                             #                                                                                                                                                   #
+        if logged(ma) == True:                                                                                                                      #
+###---------Getting user's id---------###############################################################################################################                                                                                                                      #
+            selection = ("SELECT id FROM users WHERE discord_id = ?")                                                                               #
+            userid = c.execute(selection,[(str(ma))])                                                                                               #
+                                                                                                                                                    #                              
+            msg = 'Can you tell me the code of the file you want to download?'                                                                      #
+            code = await getmsg(ma, msg)                                                                                                            #
+                                                                                                                                                    #            
+            if await checkdb('file_code', code) == True:                                                                                            #
+                file = c.execute("SELECT file_path FROM files WHERE file_code = code AND user_id = userid")                                         #
+                file = open(file)                                                                                                                   #
+                                                                                                                                                    #
+                await client.send_file(ma, file)                                                                                                    #
+                msg = 'You can always download the file by right-clicking the file and using the discord option.'                                   #
+                await sendmsg(ma, msg)                                                                                                              #
+                                                                                                                                                    #
+            else:                                                                                                                                   #
+                msg = "The file you asked for doesn't seem to exist, recheck the file code and try again."                                          #
+                await sendmsg(ma, msg)                                                                                                              #
+                await download(ma)                                                                                                                  #
+                                                                                                                                                    #
+        else:                                                                                                                                       #
+            msg = 'You are not logged in, please log in with !login before trying to download a file'                                               #
+            await sendmsg(ma, msg)                                                                                                                  #
+#                                                                                                                                                   #
+#                                                                                                                                                   #                                                                                                                                                              
+#                                                                                                                                                   #                        
+#                                                                                                                                                   #                                    
+#                                                                                                                                                   #                                                
+##########################--------This fucntion allows the user to read the files he has in his folder--------#######################################                                                                            
+async def myfiles(ma):                                                                                                                              #
+        if logged(ma) == True:                                                                                                                      #
+###---------Showing users their files---------####################################################################################################### 
+            data = c.execute("SELECT file_name, file_code FROM files WHERE user_id = userid")                                                       #
+            results = c.fetchall()                                                                                                                  #
+                                                                                                                                                    #            
+            for i in results:                                                                                                                       #
+                msg = "File name: " + i[3] + "   /   File code: " + i[2]                                                                            #
+                                                                                                                                                    #             
+                await sendmsg(ma, msg)                                                                                                              #
+                                                                                                                                                    #            
+                                                                                                                                                    #
+        else:                                                                                                                                       #
+            msg = 'You are not logged in, please log in with !login before trying to read your files'                                               #
+            await sendmsg(ma, msg)                                                                                                                  #
+#                                                                                                                                                   #
+#                                                                                                                                                   #
+#                                                                                                                                                   #
+#                                                                                                                                                   #        
+#                                                                                                                                                   #
+#                                                                                                                                                   #
+#                                                                                                                                                   #
+#                                                                                                                                                   #
+###############################################--------End of Guilherme's Code--------###############################################################                                                                                                                                                  #
 
 
 
