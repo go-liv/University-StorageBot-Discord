9b5513d8bfb66bd7e160a1b6183420e184bfbf4b
Younes RM <rastgary@coventry.ac.uk>
2018-11-14 19:30:08 +0000

    Doc Folder added

--- Bot.py
+++ Bot.py
@@ -161,7 +161,7 @@ async def getMultyMessage(ma,list):
   item = await getmsg(ma, msg)
   if item == false:
     await timesup(ma)
-  Try:
+  try:
     item = int(item)
     if await checkitem(ma,'item', item) == True:
       list.append(item)
@@ -170,7 +170,7 @@ async def getMultyMessage(ma,list):
       msg= 'item does not exitst please check the item code. to exit ingnore this msg'
       await sendmsg(ma,msg)
       getMultyMessage(ma,list)
-  expect:
+  except:
     if item.lower() == 'done':
       return list
     else:
